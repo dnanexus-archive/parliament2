6a273d3c1a7659c2d467aa8aa989abb08e1b8d77
slzarate <slzarate96@gmail.com>
2018-09-06 14:49:07 -0700

    Adding IndelRealigner calls

--- parliament2.sh
+++ parliament2.sh
@@ -42,8 +42,6 @@ if [[ "$run_breakdancer" == "False" ]] && [[ "$run_breakseq" == "False" ]] && [[
     run_delly_deletion="True"
 fi
 
-java -jar "${gatk_jar}" -version
-
 samtools faidx ref.fa &
 ref_genome=$(python /home/dnanexus/get_reference.py)
 lumpy_exclude_string=""
@@ -90,6 +88,14 @@ echo "Generate contigs"
 samtools view -H input.bam | python /getContigs.py "$filter_short_contigs" > contigs
 
 mkdir -p /home/dnanexus/out/log_files/
+
+# Frontloading IndelRealigner
+if [[ "$run_atlas" == "True" ]]; then
+    echo "Running IndelRealigner"
+    java -Xmx8G -jar "${gatk_jar}" -nt $(nproc) -T RealignerTargetCreator -R ref.fa -I input.bam -o realign.intervals -known Homo_sapiens_assembly38.dbsnp138.vcf.gz -known Homo_sapiens_assembly38.known_indels.vcf.gz -known Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+
+    java -Xmx8G -jar GenomeAnalysisTK.jar -T IndelRealigner -R ref.fa -I input.bam -targetIntervals realign.intervals -L chr1 -L chr2 -L chr3 -L chr4 -L chr5 -L chr6 -L chr 7 -L chr8 -L chr9 -L chr10 -L chr11 -L chr12 -L chr13 -L chr14 -L chr15 -L chr16 -L chr17 -L chr18 -L chr19 -L chr20 -L chr21 -L chr22 -L chrX -L chrY -L chrM -known Homo_sapiens_assembly38.dbsnp138.vcf.gz -known Homo_sapiens_assembly38.known_indels.vcf.gz -known Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+fi
 
 # Frontloading xAtlas
 if [[ "$run_stats" == "True" ]]; then
