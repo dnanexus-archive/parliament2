c2d655b5798c28f047f1017f05b6585c64bc4131
slzarate <slzarate96@gmail.com>
2018-09-13 11:25:26 -0700

    Cleaning up code, renaming to reflect CCDG

--- parliament2.sh
+++ parliament2.sh
@@ -99,7 +99,6 @@ if [[ "$run_atlas" == "True" ]]; then
 
     echo "Running IndelRealigner"
     java -Xmx8G -jar "${gatk_jar}" -nt $(nproc) -T IndelRealigner -R ref.fa -I input.bam -targetIntervals realign.intervals -L chr1 -L chr2 -L chr3 -L chr4 -L chr5 -L chr6 -L chr7 -L chr8 -L chr9 -L chr10 -L chr11 -L chr12 -L chr13 -L chr14 -L chr15 -L chr16 -L chr17 -L chr18 -L chr19 -L chr20 -L chr21 -L chr22 -L chrX -L chrY -L chrM -known Homo_sapiens_assembly38.dbsnp138.vcf.gz -known Homo_sapiens_assembly38.known_indels.vcf.gz -known Mills_and_1000G_gold_standard.indels.hg38.vcf.gz -o indel_realigned.bam &
-
     indel_realigner_pid=$!
 fi
 
