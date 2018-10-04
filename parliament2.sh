d8ff66817565841902bd95a57ba7d2ae5f04fca2
slzarate <slzarate96@gmail.com>
2018-10-04 09:56:26 -0700

    Remove empty log directories at the end (may create more than necessary)

--- parliament2.sh
+++ parliament2.sh
@@ -636,4 +636,5 @@ if [[ "${run_genotype_candidates}" == "True" ]]; then
     fi
 fi
 
-find /home/dnanexus/out/log_files/ -maxdepth 1 -mindepth 1 -type d -exec tar czvf {}.tar.gz {} --remove-files \;
+find /home/dnanexus/out/log_files/ -maxdepth 1 -mindepth 1 -type d -exec tar czvf {}.tar.gz {} --remove-files
+find /home/dnanexus/out/log_files/ -type d -empty -delete
