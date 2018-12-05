c1d3ec1f6a4e5633fb40113bfa4ed5316ef4169f
slzarate <slzarate96@gmail.com>
2018-12-04 17:03:39 -0800

    Modify test

--- test_scripts/small_test.sh
+++ test_scripts/small_test.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-wget https://wiki.dnanexus.com/images/files/dx-toolkit-current-ubuntu-14.04-amd64.tar.gz
+wget -q https://wiki.dnanexus.com/images/files/dx-toolkit-current-ubuntu-14.04-amd64.tar.gz
 tar zxf dx-toolkit-current-ubuntu-14.04-amd64.tar.gz
 source dx-toolkit/environment
 
+mkdir -p /home/dnanexus/in
+mkdir -p /home/dnanexus/out
+
 dx login --token $DX_AUTH_TOKEN --noprojects
 # Download small input BAM and index
-dx download file-By3JP400k0B037Jpv1Jq856f -o small_input.bam
-dx download file-Bzp2vG80X3VQv24XgQx0Qy1J -o small_input.bai
+dx download file-By3JP400k0B037Jpv1Jq856f -o /home/dnanexus/small_input.bam
+dx download file-Bzp2vG80X3VQv24XgQx0Qy1J -o /home/dnanexus/small_input.bai
 # Download reference FASTA and index
-dx download file-B6ZY7VG2J35Vfvpkj8y0KZ01 -o ref.fa.gz
-dx download file-ByGVY4j04Y0YJ0yJpj0f8qPG -o ref.fa.fai
-docker run dnanexus/parliament2:$TAG --bam small_input.bam --ref_genome ref.fa.gz --prefix breakdancer --breakdancer 
+dx download file-B6ZY7VG2J35Vfvpkj8y0KZ01 -o /home/dnanexus/ref.fa.gz
+dx download file-ByGVY4j04Y0YJ0yJpj0f8qPG -o /home/dnanexus/ref.fa.fai
+docker run dnanexus/parliament2:$TAG -v /home/dnanexus/in:/home/dnanexus/in -v /home/dnanexus/out>:/home/dnanexus/out --bam small_input.bam --ref_genome ref.fa.gz --prefix breakdancer --breakdancer 
