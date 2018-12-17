b79978698aef4a93910b1f92f284ee07c673e0f8
slzarate <slzarate96@gmail.com>
2018-12-17 14:36:01 -0800

    debug large test

--- test_scripts/hg002_test.sh
+++ test_scripts/hg002_test.sh
@@ -10,8 +10,10 @@ mkdir -p /home/dnanexus/out
 dx login --token $DX_AUTH_TOKEN
 dx select project-FQ06yF00B227FvGg5zxvk5fy
 
-echo "Building applet"
+docker run dnanexus/parliament2:$TAG -h
+
+echo "Building applet" >&1
 dx build dx_app_code/parliament2 -a | jq '.id' > applet_id.txt
 
-echo "Running applet"
+echo "Running applet" >&1
 dx run $(cat applet_id.txt) -iiluminabam="project-FQ06yF00B227FvGg5zxvk5fy:file-Bg0b9F00pJZQV695v18Ykf7K" -iref_fasta="project-FQ06yF00B227FvGg5zxvk5fy:file-B6ZY7VG2J35Vfvpkj8y0KZ01" -iref_index="project-FQ06yF00B227FvGg5zxvk5fy:file-ByGVY4j04Y0YJ0yJpj0f8qPG" -iprefix="large_test" -y
