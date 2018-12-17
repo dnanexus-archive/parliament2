aa75b41f813f5465d5a8b316c2e3af95f04cd360
slzarate <slzarate96@gmail.com>
2018-12-17 09:34:43 -0800

    Updating large test script

--- test_scripts/hg002_test.sh
+++ test_scripts/hg002_test.sh
@@ -10,6 +10,8 @@ mkdir -p /home/dnanexus/out
 dx login --token $DX_AUTH_TOKEN
 dx select project-FQ06yF00B227FvGg5zxvk5fy
 
+echo "Building applet"
 dx build dx_app_code/parliament2 -a | jq '.id' > applet_id.txt
 
-dx run $(cat applet_id.txt) -iiluminabam="project-FQ06yF00B227FvGg5zxvk5fy:file-Bg0b9F00pJZQV695v18Ykf7K" -iref_fasta="project-FQ06yF00B227FvGg5zxvk5fy:file-B6ZY7VG2J35Vfvpkj8y0KZ01" -iref_index="project-FQ06yF00B227FvGg5zxvk5fy:file-ByGVY4j04Y0YJ0yJpj0f8qPG" -iprefix="large_test" 
+echo "Running applet"
+dx run $(cat applet_id.txt) -iiluminabam="project-FQ06yF00B227FvGg5zxvk5fy:file-Bg0b9F00pJZQV695v18Ykf7K" -iref_fasta="project-FQ06yF00B227FvGg5zxvk5fy:file-B6ZY7VG2J35Vfvpkj8y0KZ01" -iref_index="project-FQ06yF00B227FvGg5zxvk5fy:file-ByGVY4j04Y0YJ0yJpj0f8qPG" -iprefix="large_test" -y
