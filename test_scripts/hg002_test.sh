93f915c9e25597444c2efa78ca57d380a9b149ec
slzarate <slzarate96@gmail.com>
2018-12-18 10:31:30 -0800

    Updating large script

--- test_scripts/hg002_test.sh
+++ test_scripts/hg002_test.sh
@@ -16,4 +16,4 @@ echo "Building applet" >&1
 dx build dx_app_code/parliament2 -a | jq '.id' > applet_id.txt
 
 echo "Running applet" >&1
-dx run $(cat applet_id.txt) -iiluminabam="project-FQ06yF00B227FvGg5zxvk5fy:file-Bg0b9F00pJZQV695v18Ykf7K" -iref_fasta="project-FQ06yF00B227FvGg5zxvk5fy:file-B6ZY7VG2J35Vfvpkj8y0KZ01" -iref_index="project-FQ06yF00B227FvGg5zxvk5fy:file-ByGVY4j04Y0YJ0yJpj0f8qPG" -iprefix="large_test" -y
+dx run $(cat applet_id.txt) -iiluminabam="project-FQ06yF00B227FvGg5zxvk5fy:file-Bg0b9F00pJZQV695v18Ykf7K" -iref_fasta="project-FQ06yF00B227FvGg5zxvk5fy:file-B6ZY7VG2J35Vfvpkj8y0KZ01" -iref_index="project-FQ06yF00B227FvGg5zxvk5fy:file-ByGVY4j04Y0YJ0yJpj0f8qPG" -iprefix="large_test" --yes --brief --wait --watch
